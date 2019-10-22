24c338b0f05e11bab5eb14c0edc6928047227dc2
Duncan Robbins and Saul Ordonez <duncan+saul@>
2019-10-22 14:31:47 -0700

    add the first row of flash cards

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+  $(".clickable1").click(function(){
+    $("#initial-hidden1").toggle();
+    $("#initial-showing1").toggle();
+  });
+
+  $(".clickable2").click(function(){
+    $("#initial-hidden2").toggle();
+    $("#initial-showing2").toggle();
+  });
+
+  $(".clickable3").click(function(){
+    $("#initial-hidden3").toggle();
+    $("#initial-showing3").toggle();
+  });
+
+  $(".clickable4").click(function(){
+    $("#initial-hidden4").toggle();
+    $("#initial-showing4").toggle();
+  });
+
+
+
+});
