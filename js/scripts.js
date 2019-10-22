7116320f06c4887da8d34e9da1b78725c0dae6a0
Duncan Robbins and Saul Ordonez <duncan+saul@>
2019-10-22 14:42:27 -0700

    add second row with appropriate js and css

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,25 @@ $(document).ready(function(){
     $("#initial-showing4").toggle();
   });
 
+  $(".clickable5").click(function(){
+    $("#initial-hidden5").toggle();
+    $("#initial-showing5").toggle();
+  });
+
+  $(".clickable6").click(function(){
+    $("#initial-hidden6").toggle();
+    $("#initial-showing6").toggle();
+  });
+
+  $(".clickable7").click(function(){
+    $("#initial-hidden7").toggle();
+    $("#initial-showing7").toggle();
+  });
+
+  $(".clickable8").click(function(){
+    $("#initial-hidden8").toggle();
+    $("#initial-showing8").toggle();
+  });
 
 
 });
